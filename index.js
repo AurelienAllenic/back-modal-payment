589b0f2fb21ee42530098c4edc7fc62a6d6ffa97
AurelienAllenic <aurelien.allenic@gmail.com>
2025-11-25 14:12:00 +0100

    (feat) added vercel deployment of modal payment to cors config

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174"] // au cas où tu changes de port
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174", "https://modal-payment.vercel.app/"] // au cas où tu changes de port
 }));
 app.use(express.json());
 
